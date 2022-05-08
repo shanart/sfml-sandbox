896c081f44f01cdc07d946f62261ad12e2aed369
shanart <shanin.x@gmail.com>
2022-05-08 23:49:43 +0300

    Selection rectangle

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+sf::Vector2f setRectanglePosition(sf::Vector2i position);
+
+int wWidth = 640;
+int wHeight = 480;
+
+int main()
+{
+    // Main window
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "First window");
+
+    // objects
+    sf::RectangleShape rectangle(sf::Vector2f(0, 0));
+    rectangle.setFillColor(sf::Color::Green);
+
+    bool drawing = false;
+    sf::Vector2f rSize = sf::Vector2f(0.0f, 0.0f);
+    sf::Vector2f startPosition = sf::Vector2f(0.0f, 0.0f);
+    sf::Vector2f endPosition = sf::Vector2f(0.0f, 0.0f);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            else if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                if (drawing == false)
+                {
+                    startPosition = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
+                    drawing = true;
+                }
+            }
+
+            else if (event.mouseButton.button == sf::Mouse::Left && event.type == sf::Event::MouseButtonReleased)
+            {
+                std::cout << "\nsX: " << startPosition.x << ", sY: " << startPosition.y << std::endl;
+                std::cout << "eX: " << endPosition.x << ", eY: " << endPosition.y << std::endl;
+                drawing = false;
+            }
+        }
+
+        if (drawing == true)
+        {
+            sf::Vector2i mp = sf::Mouse::getPosition(window);
+            std::cout << mp.x << ", " << mp.y << std::endl;
+            endPosition = static_cast<sf::Vector2f>(mp);
+
+            rSize.x = endPosition.x - startPosition.x;
+            rSize.y = endPosition.y - startPosition.y;
+        }
+
+        window.clear(sf::Color::Black);
+
+        rectangle.setSize(rSize);
+        rectangle.setPosition(startPosition);
+
+        window.draw(rectangle);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+g++ main.cpp -lsfml-graphics -lsfml-window -lsfml-system -o main &&
+./main
